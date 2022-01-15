bd3716ad2a13799e1303ec8c1384fcc9d3d330b9
DIDRICH Damien <89045920+skunkoff1@users.noreply.github.com>
2022-01-15 23:03:20 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="./ressources/css/style.css">
+    </head>
+
+    <body>
+        <header>
+            <div id="title">
+                <h1>Jeu de mémoire</h1>
+            </div>
+            <div id="buttonDiv">
+                <div class=button>Easy</div>
+                <div class=button>Medium</div>
+                <div class=button>Hard</div>
+                <div class=button>Impossible</div>
+            </div>
+        </header>
+        <div id="gameContainer">
+            <div id="table">
+                
+            </div>
+        </div>
+
+        <script src="./ressources/js/script.js"></script>
+    </body>
+</html>
